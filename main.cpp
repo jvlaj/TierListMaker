74a527d0c8f1d0607c7beae025fc71565dd54ae3
jvlaj <jvlaj@protonmail.com>
2021-01-04 15:04:33 +1100

    now iterates through XML file given and saves name & rating data into an array. tiers need to be fixed - possibly need some sort of algorithm for tiers to the relative score.

--- main.cpp
+++ main.cpp
@@ -6,47 +6,70 @@
 #include "tinyxml2/tinyxml2.cpp"
 #include "tinyxml2/tinyxml2.h"
 
-std::string parseData(FILE *fp);
-
 enum Ratings { S, A, B, C, D, E, F };
 
 struct Show {
-    std::string name;
-    const char *score;
+    const char *name;
+    int score;
     Ratings tier;
 };
 
+std::array<Show, 1000> parseData(FILE *fp);
+
 int main(int argc, char *argv[]) {
     std::cout << "Loading Anime List.\n";
-    FILE *myfile;
-    myfile = fopen("mylist.xml", "r");
 
-    std::string parsedData = parseData(myfile);
-
-    return 0;
-}
-
-std::string parseData(FILE *fp) {
     tinyxml2::XMLDocument doc;
-    doc.LoadFile(fp);  // load the file;
-    tinyxml2::XMLDocument watchedAnime;
+    FILE *myfile;
+    myfile = fopen("/home/jason/Code/TierListMaker/mylist.xml", "r");
+    doc.LoadFile(myfile);
 
-    std::array<Show, 20> arrayOfShows;  // array of structs
+    std::array<Show, 1000> arrayOfShows;
 
-    short count;
-    for (count = 0; count < 10; count++) {
-        if (doc.NextSiblingElement("anime")
-                ->NextSiblingElement("is_watched")
-                ->GetText()) {
-            arrayOfShows[count].score =
-                doc.NextSiblingElement("my_rating")->GetText();
+    tinyxml2::XMLElement *pEntryElement = doc.FirstChildElement("mylist")
+                                              ->FirstChildElement("anime")
+                                              ->FirstChildElement("entry");
+
+    for (int count = 0; pEntryElement != nullptr; count++) {
+        if (pEntryElement->BoolAttribute("is_watched")) {
             arrayOfShows[count].name =
-                doc.NextSiblingElement("name")->GetText();
-            // std::cout << arrayOfShows[count].name << "\n"
-            //<< arrayOfShows[count].score << "\n";
+                pEntryElement->FirstChildElement("name")->GetText();
+            arrayOfShows[count].score =
+                pEntryElement->IntAttribute("my_rating");
+            std::cout << "Name: " << arrayOfShows[count].name << "\n";
+            std::cout << "Score: " << arrayOfShows[count].score << "\n";
+            switch (arrayOfShows[count].score) {
+                case 9:
+                    arrayOfShows[count].tier = S;
+                    break;
+                case 8:
+                    arrayOfShows[count].tier = A;
+                    break;
+                case 7:
+                    arrayOfShows[count].tier = B;
+                    break;
+                case 6:
+                    arrayOfShows[count].tier = C;
+                    break;
+                case 5:
+                    arrayOfShows[count].tier = D;
+                    break;
+                case 4:
+                    arrayOfShows[count].tier = E;
+                    break;
+                case 3:
+                    arrayOfShows[count].tier = F;
+                    break;
+                case 2:
+                    arrayOfShows[count].tier = F;
+                    break;
+                case 1:
+                    arrayOfShows[count].tier = F;
+                    break;
+            }
+            std::cout << "tier: " << arrayOfShows[count].tier << "\n";
         }
-        doc.NextSibling();
+        pEntryElement = pEntryElement->NextSiblingElement();
     }
-    std::cout << arrayOfShows[1].score;
     return 0;
 }
