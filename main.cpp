0f37876d280842d906a92943aa081fb6a260367e
jvlaj <jvlaj@protonmail.com>
2021-01-05 13:33:57 +1100

    Now prints tier lists for all watched anime in XML file. Still uses staic algorithm, may need to update to dynamic in the future.

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 #include "tinyxml2/tinyxml2.cpp"
 #include "tinyxml2/tinyxml2.h"
 
-enum Ratings { S, A, B, C, D, E, F };
+enum Tiers { S, A, B, C, D, E, F };
 
 struct Show {
     const char *name;
     int score;
-    Ratings tier;
+    Tiers tier;
 };
 
-std::array<Show, 1000> parseData(FILE *fp);
+void print_tiers(std::array<Show, 1000>);
 
 int main(int argc, char *argv[]) {
     std::cout << "Loading Anime List.\n";
@@ -36,8 +36,8 @@ int main(int argc, char *argv[]) {
                 pEntryElement->FirstChildElement("name")->GetText();
             arrayOfShows[count].score =
                 pEntryElement->IntAttribute("my_rating");
-            std::cout << "Name: " << arrayOfShows[count].name << "\n";
-            std::cout << "Score: " << arrayOfShows[count].score << "\n";
+            // std::cout << "Name: " << arrayOfShows[count].name << "\n";
+            // std::cout << "Score: " << arrayOfShows[count].score << "\n";
             switch (arrayOfShows[count].score) {
                 case 9:
                     arrayOfShows[count].tier = S;
@@ -66,10 +66,56 @@ int main(int argc, char *argv[]) {
                 case 1:
                     arrayOfShows[count].tier = F;
                     break;
+
+                default:
+                    arrayOfShows[count].tier = F;
+                    break;
             }
-            std::cout << "tier: " << arrayOfShows[count].tier << "\n";
+            // std::cout << "tier: " << arrayOfShows[count].tier << "\n";
+        } else {
+            count--;
         }
         pEntryElement = pEntryElement->NextSiblingElement();
     }
+    print_tiers(arrayOfShows);
     return 0;
 }
+
+void print_tiers(std::array<Show, 1000> arrayOfShows) {
+    for (int i = 0; i <= 6; i++) {
+        switch (i) {
+            case 0:
+                std::cout << "S:  ";
+                break;
+            case 1:
+                std::cout << "A:  ";
+                break;
+            case 2:
+                std::cout << "B:  ";
+                break;
+            case 3:
+                std::cout << "C:  ";
+                break;
+            case 4:
+                std::cout << "D:  ";
+                break;
+            case 5:
+                std::cout << "E:  ";
+                break;
+            case 6:
+                std::cout << "F:  ";
+                break;
+        }
+        for (int a = 0; arrayOfShows[a].name != nullptr; a++) {
+            if (arrayOfShows[a].tier == i) {
+                std::cout << "|  ";
+                std::cout << arrayOfShows[a].name;
+                // std::cout << ": ";
+                // std::cout << arrayOfShows[a].score;
+                std::cout << "  ";
+            }
+        }
+        std::cout << "\n\n";
+    }
+}
+
